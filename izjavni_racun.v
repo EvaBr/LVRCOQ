a62f4790eeaf3f6ef3da0d6a2d07f0a3614b3e6d
EvaBr <eva.breznik@student.fmf.uni-lj.si>
2014-04-25 01:41:17 +0200

    Update-anje.

---
+++ izjavni_racun.v
@@ -0,0 +1,103 @@
+Parameter A B C : Prop.
+
+Theorem kr_en: A /\ B -> B /\ A.
+Proof.
+  intro.
+  split.
+  - destruct H.
+    assumption.
+  - destruct H.
+    assumption.
+Qed.
+
+Theorem kr_en_drugic: A /\ B -> B /\ A.
+Proof.
+  tauto.
+Qed.
+
+Theorem vaja1: A -> (B -> A).
+Proof.
+  intro.
+  intro.
+  assumption.
+Qed.
+
+Theorem vaja3: A \/ A -> A.
+Proof.
+  intros G.
+  destruct G.
+  - assumption.
+  - assumption.
+Qed.
+
+Theorem vaja3': A \/ A -> A.
+Proof.
+  intros [G1|G2]. (*Kombiniran intro-destruct, kjer poves kako naj destructa - na G1 in G2.*)
+  - assumption.
+  - assumption.
+Qed.
+
+Theorem vaja3'': A \/ A -> A.
+Proof.
+  intros [?|?]. (*Sam poimenuje hipoteze. Ce das not _, pa dano hipotezo ignorira.*)
+  - assumption.
+  - assumption.
+Qed.
+
+Theorem vaja4: (A \/ B -> C) -> (A -> C) /\ (B -> C).
+Proof.
+  intro.
+  split.
+  - intro.
+    apply H.
+    left.
+    assumption.
+  - intro.
+    apply H.
+    right.
+    assumption.
+Qed.
+
+Theorem vaja5: (A -> C) /\ (B -> C) -> (A \/ B -> C).
+Proof.
+  intros [H1 H2] [G1|G2]. (* intros. destruct H. destruct G. *)
+  - apply H1.
+    assumption.
+  - apply H2.
+    assumption.
+Qed.
+
+Theorem vaja8: ~ (A \/ B) -> ~ A /\ ~ B.
+Proof.
+  intro.
+  split.
+  - intro.
+    absurd (A \/ B). (* ktera izjava je taka, da bomo dokazal izj in ne izj. rabmo jo pa zto, kr hocmo F dokazat. *)
+    + assumption.
+    + left.
+      assumption.
+  - intro.
+    absurd (A \/ B).
+    + assumption.
+    + right.
+      assumption.
+Qed.
+
+
+Theorem vaja7: A -> ~ ~ A.
+Proof.
+  intros H G. (* s tem ga prsilis da bo dva introja nardu. Sicer jih neb, k sam od sebe ne uposteva def. negacije. *)
+  apply G.
+  assumption.
+Qed.
+
+
+Print vaja7.
+
+
+
+
+
+
+
+  
--- predikatni_racun.v
+++ predikatni_racun.v
@@ -1,41 +1,64 @@
 (* Osnovne vaje iz logike 1. reda. *)
 
-(* Uporabimo knižnico, ki ima taktiko omega. Ta zna delati z naravnimi števili,
+(* Uporabimo knjižnico, ki ima taktiko omega. Ta zna delati z naravnimi števili,
    če nastopajo samo linearne funkcije. *)
 Require Import Omega.
 
-(* Tip naravnih števil je nat. Ima element 0, operacijo naslednik S in običjane
+(* Tip naravnih števil je nat. Ima element 0, operacijo naslednik S in običajne
    aritmetične operacije. *)
 
 Theorem vaja_1:
   forall n : nat, exists m : nat, n < m.
 Proof.
-  (* naredimo na predavanjih *)
-  admit.
+  intro.
+  exists (S n). (* (S n) - naslednik od n. *)
+  omega.
+  (* admit - ce se bos na ta del se kasneje vrnil, za zdaj pa ga naj smatra kot 
+     dokazanega. *)
 Qed.
 
 Theorem vaja_2 (n : nat): exists m : nat, n < m.
 Proof.
-  (* naredimo na predavanjih *)
-  admit.
+  exists (1 + n).
+  omega.  (* Tle bi delal tudi auto. Ne bi pa delal, ce bi pisali exists (n + 1),
+             saj je sestevanje definirano s pomocjo rekurzije na prvem clenu. *)
 Qed.
 
 Theorem vaja_4 (n m : nat): 2 * n + m + 1 = m + n + 1 + n.
 Proof.
-  (* naredimo na predavanjih *)
-  admit.
+  omega.
 Qed.
 
 Theorem vaja_5 (n : nat): (exists m : nat, n = 2 * m) \/ (exists m : nat, n = 1 + 2 * m).
 Proof.
-  (* naredimo na predavanjih *)
-  admit.
+  induction n.
+  - left.
+    exists 0.
+    auto.
+  - destruct IHn.
+    + right.
+      destruct H.
+      exists x.
+      auto.
+    + left.
+      destruct H.
+      exists (S x).
+      omega.
 Qed.
 
 Theorem vaja_6: forall n, exists m, n = 3 * m \/ n = 3 * m + 1 \/ n = 3 * m + 2.
 Proof.
-  (* naredimo na vajah *)
-  admit.
+  intro.
+  induction n.
+  - exists 0.
+    left.
+    auto.
+  - destruct IHn as [m [Babitty|[Rabitty|Blop]]].
+    + exists m.
+      right; left.
+      omega.
+    + exists m; right; right; omega.
+    + exists (S m); left; omega.
 Qed.
 
 (* Predpostavimo, da imamo množici A in B. *)
@@ -48,8 +71,10 @@ Parameter Q : B -> Prop.
 Theorem vaja_7:
   forall y : B, (forall x : A , P x /\ Q y) -> (forall z : A, P z).
 Proof.
-  (* naredimo na predavanjih *)
-  admit.
+  intro.
+  intro.
+  intro x.
+  apply H.
 Qed.
 
 (* Predpostavimo relacijo R med A in B. *)
@@ -58,15 +83,28 @@ Parameter R : A -> B -> Prop.
 Theorem vaja_8:
   (exists x : A, forall y : B, R x y) -> (forall y : B, exists x : A, R x y).
 Proof.
-  (* naredimo na vajah *)
-  admit.
+  intro.
+  intro.
+  destruct H.
+  exists x.
+  apply H.
 Qed.
 
 Theorem vaja_9:
   ~ (exists x : A, P x) <-> forall x : A, ~ P x.
 Proof.
-  (* naredimo na vajah *)
-  admit.
+  split.
+  - intros.
+    intro.
+    absurd (exists x : A, P x).
+    + assumption.
+    + exists x; apply H0.
+  - intros.
+    intro.
+    destruct H0 as [x0 G].
+    absurd  (P x0).
+    + apply H.
+    + assumption.
 Qed.
 
 (* Zakon o izključeni tretji možnosti. 
@@ -78,8 +116,14 @@ Definition dn := forall P : Prop, ~ ~ P -> P.
 
 Lemma vaja_10: lem -> dn.
 Proof.
-  (* naredimo na vajah *)
-  admit.
+  unfold lem, dn.
+  intro.
+  intros P.
+  intro.
+  
+  apply H.
+  intro.
+  absurd 
 Qed.
 
 Lemma vaja_11: dn -> lem.
